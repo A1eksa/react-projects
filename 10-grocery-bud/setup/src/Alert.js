ffa94785f91f4e0796b06ce0953d860547a16c3a
Aleksandra Safranko <aleksandra.safranko1@gmail.com>
2022-04-26 00:45:38 +0200

    shopping list app

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
-const Alert = () => {
-  return <h2>alert component</h2>
-}
+const Alert = ({ type, msg, removeAlert }) => {
+  console.log(removeAlert, 'remove');
 
-export default Alert
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
+
+  console.log(name, 'name');
+  console.log(list, 'list');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      //display alert
+      showAlert(true, 'danger', 'please enter value');
+      // setAlert({ show: true, msg: 'Please enter value', type: 'danger' });
+    } else if (name && isEditing) {
+      //deal wit editing
+    } else {
+      //show alert
+      showAlert(true, 'success', 'item added to the list');
+      //create new item
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
+  };
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        <h3>Grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='my shopping list'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn'>Clear items</button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
