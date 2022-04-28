f7ee7f61c4684d0f554b0a496a2b159dc9474bae
Aleksandra Safranko <aleksandra.safranko1@gmail.com>
2022-04-28 21:47:33 +0200

    finished navbar, amazing useRef omggit add .git add .

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,6 +5,18 @@ import logo from './logo.svg';
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    // console.log(linksHeight);
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className='nav-center'>
@@ -17,7 +29,39 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
+        {/* solution with useRef */}
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {/* second solution with state and classes 
+        <div
+          className={`${
+            showLinks ? 'links-container show-container' : 'links-container'
+          }`}
+        >
+          <ul className='links'>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div> */}
+
+        {/* solution with using state then condition */}
+        {/* {showLinks && (
           <div className='links-container show-container'>
             <ul className='links'>
               {links.map((link) => {
@@ -30,7 +74,7 @@ const Navbar = () => {
               })}
             </ul>
           </div>
-        )}
+        )} */}
 
         <ul className='social-icons'>
           {social.map((media) => {
